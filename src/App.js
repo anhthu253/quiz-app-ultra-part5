5cb911a3b58efc19bd3bb10c6b951ac1d1015fc0
ThomasF85 <thomas.foeldi@gmail.com>
2022-09-20 12:12:34 +0200

    remove uuid dependency and usage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { nanoid } from "nanoid";
 import "./App.css";
 import Card from "./components/card/Card";
 import Header from "./components/header/Header";
@@ -6,19 +5,19 @@ import Navigation from "./components/navigation/Navigation";
 
 const cards = [
   {
-    id: nanoid(),
+    id: "b5db267b-3275-4a86-a9f4-e7f927d33ed0",
     question: "Question 1",
     answer: "Answer 1",
     tags: ["Tag 1a", "Tag 1b", "Tag 1c"],
   },
   {
-    id: nanoid(),
+    id: "8f76114b-c30c-411d-ad38-77ce69079eef",
     question: "Question 2",
     answer: "Answer 2",
     tags: ["Tag 2a", "Tag 2b", "Tag 2c"],
   },
   {
-    id: nanoid(),
+    id: "8c74c87b-2a68-4aa1-9733-4c6917dfdf88",
     question: "Question 3",
     answer: "Answer 3",
     tags: ["Tag 3a", "Tag 3b", "Tag 3c"],
